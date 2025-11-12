d303c3f98ca48cd902f508883a4aac4acee42c43
eMacharia001 <machariasensei@gmail.com>
2025-11-13 00:19:15 +0300

    Add task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -2,6 +2,6 @@
 -- Insert a single customer into the customer table
 
 USE alx_book_store;
-
+DELETE FROM Customers WHERE customer_id = 1;
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
